27928c26fee15cb477cad10f1788e54b04093c45
phiychai <3500928+phiychai@users.noreply.github.com>
2025-12-10 19:20:13 +0000

    chore(types): update Payload generated types
    - Update auto-generated Payload types file

--- packages/shared-types/src/payload-types.ts
+++ packages/shared-types/src/payload-types.ts
@@ -101,7 +101,9 @@ export interface Config {
     'form-submissions': FormSubmissionsSelect<false> | FormSubmissionsSelect<true>;
     'form-submission-values': FormSubmissionValuesSelect<false> | FormSubmissionValuesSelect<true>;
     'payload-kv': PayloadKvSelect<false> | PayloadKvSelect<true>;
-    'payload-locked-documents': PayloadLockedDocumentsSelect<false> | PayloadLockedDocumentsSelect<true>;
+    'payload-locked-documents':
+      | PayloadLockedDocumentsSelect<false>
+      | PayloadLockedDocumentsSelect<true>;
     'payload-preferences': PayloadPreferencesSelect<false> | PayloadPreferencesSelect<true>;
     'payload-migrations': PayloadMigrationsSelect<false> | PayloadMigrationsSelect<true>;
   };
@@ -595,7 +597,15 @@ export interface FormField {
   /**
    * Input type for the field
    */
-  type: 'text' | 'textarea' | 'checkbox' | 'checkbox_group' | 'radio' | 'file' | 'select' | 'hidden';
+  type:
+    | 'text'
+    | 'textarea'
+    | 'checkbox'
+    | 'checkbox_group'
+    | 'radio'
+    | 'file'
+    | 'select'
+    | 'hidden';
   /**
    * Default text shown in empty input.
    */
@@ -1331,7 +1341,6 @@ export interface NavigationSelect<T extends boolean = true> {
 export interface Auth {
   [k: string]: unknown;
 }
-
 
 declare module 'payload' {
   export interface GeneratedTypes extends Config {}
